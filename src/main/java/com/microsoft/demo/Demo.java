fd8f2e97d1a68b89ccacd890881aff1e2cd774dd
sahil23kr <kumar.sahil917@gmail.com>
2025-01-08 12:09:01 +0530

    added new line for checking pipeline

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,6 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
+            System.out.println("checking pipeline");
             System.out.println("I am covered");
             return;
         }
